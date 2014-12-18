58623f39a83aeb9a31cb8742a4745bcfcc61f697
Jared Hirsch <ohai@6a68.net>
2014-12-18 14:19:40 -0800

    super ugly / barely works...but works

--- server/index.js
+++ server/index.js
@@ -10,15 +10,7 @@ var config = require('./config');
 var log = require('./logger')('server.index');
 var routes = require('./routes');
 
-var server = new Hapi.Server({
-  connections: {
-    routes: {
-      files: {
-        relativeTo: path.join(__dirname, '../', config.get('server.staticPath'))
-      }
-    }
-  }
-});
+var server = new Hapi.Server({ debug: { request: ['error'] } });
 server.connection({
   host: config.get('server.host'),
   port: config.get('server.port')
@@ -31,32 +23,40 @@ server.register([require('hapi-auth-cookie'), require('bell')], function (err) {
   }
 
   // hapi-auth-cookie init
+  // TODO: when we really have to, move this crap into a config file
   server.auth.strategy('session', 'cookie', {
-    password: config.get('server.session.password'),
+    password: 'cookie_encryption_password',
+    cookie: 'sid-chronicle',
     redirectTo: '/',
-    isSecure: config.get('server.session.isSecure'),
-    ttl: config.get('server.session.duration')
+    isSecure: false, 
+    ttl: 1000 * 60 * 60 * 24 * 7
   });
 
   // bell init
+  // TODO move into a config file only when we actually have to 
   server.auth.strategy('oauth', 'bell', {
     provider: {
-      protocol: config.get('server.oauth.protocol'),
-      auth: config.get('server.oauth.authEndpoint'),
-      token: config.get('server.oauth.tokenEndpoint'),
-      version: config.get('server.oauth.version'),
-      scope: config.get('server.oauth.scope'),
-      /*jshint unused:false */
+      protocol: 'oauth2',
+      auth: 'https://oauth-latest.dev.lcip.org/v1/authorization',
+      token: 'https://oauth-latest.dev.lcip.org/v1/token',
+      version: '2.0',
+      scope: ['chronicle','profile'],
       profile: function (credentials, params, get, profileCb) {
+        throw new Error('inside bell provider profile function');
+        log.info('inside the oauth-bell profile callback!');
+        log.info('credentials: ' + credentials);
+        log.info('params: ' + JSON.stringify(params));
+        log.info('get: ' + get);
+        log.info('profileCb: ' + profileCb);
         // TODO here's a guess at what to do in here, bell provides no example:
         // 1. grab params.token, send it to the profile endpoint
         // 2. put the profile response into the credentials object, then send it to DB
       }
     },
-    password: config.get('server.session.password'),
-    clientId: config.get('server.oauth.clientId'),
-    clientSecret: config.get('server.oauth.clientSecret'),
-    isSecure: config.get('server.session.isSecure')
+    password: 'cookie_encryption_password',
+    clientId: '1f9bbddcb3e160ab',
+    clientSecret: '24bf8caeaa685e2e42d9a75b48511f83adffaf6fcdd174ce8749358a376be911',
+    isSecure: false
   });
 
   server.route(routes);
--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,11 @@
 
 'use strict';
 
+var path = require('path');
+var wreck = require('wreck');
+var uuid = require('uuid');
+
+var log = require('./logger')('server.routes');
 var config = require('./config');
 
 module.exports = [{
@@ -17,7 +22,12 @@ module.exports = [{
     plugins: { 'hapi-auth-cookie': { redirectTo: false } },
     handler: function (request, reply) {
       var page = request.auth.isAuthenticated ? 'app.html' : 'index.html';
-      reply.file(page);
+      // TODO should we set a cookie here? yup.
+      // TODO: but don't we want to only cookie logged-in users?
+      //       and isn't it silly to pass the uuid to the client-side to use as the nonce?
+      // request.auth.session.set({temporarySession: uuid.v4()});
+      // TODO we need a way to inject the temporarySessionId into the page
+      reply.file(path.join(__dirname, '../app', page));
     }
   }
 }, {
@@ -31,18 +41,59 @@ module.exports = [{
   method: 'GET',
   path: '/auth/complete',
   config: {
-    auth: 'oauth',
+    auth: {
+      strategy: 'oauth',
+      mode: 'try'
+    },
     handler: function (request, reply) {
-      // TODO: bell did the oauth thing for us. assume it's in the request.
-      // next, hit the profile server to get the fxa_uid, email, name, avatar.
-      // finally, set the fxa_uid in the cookie,
-      // then hapi-auth-cookie signs the cookie.
-      var sessionData = {
-        fxaId: '6d940dd41e636cc156074109b8092f96'
+      // at this point, the oauth dance is complete, we have a code but not a token.
+      // we need to swap the code for the token,
+      // then we need to ask the profile server for the user's profile,
+      // then we need to save the session.
+      // TODO: maybe we want this to live inside the server.auth.strategy call for bell?
+      log.info('auth/complete invoked');
+      log.info('do we have query params? ' + JSON.stringify(request.query));
+      // HUGE TODO: we really want to verify this wasn't replayed etc. use the 'state'. skipping for now.
+
+      // 1. we have the code. POST to /v1/token.
+      // request params: client_id, client_secret, code.
+      // response params: access_token, scope, token_type (currently 'bearer' always)
+      var tokenPayload = {
+        client_id: config.get('server.oauth.clientId'),
+        client_secret: config.get('server.oauth.clientSecret'),
+        code: request.query.code
       };
-      reply()
-        .state('session', sessionData)
-        .redirect('/'); //redirect home, then we'll load the logged-in view
+      log.info('about to post the following stuff to thetoken endpoint: ' + JSON.stringify(tokenPayload));
+      // TODO use promises to flatten this pyramid
+      wreck.post(config.get('server.oauth.tokenEndpoint'),
+        { payload: JSON.stringify(tokenPayload) },
+        function(err, res, payload) {
+          // kk so, what did we get?
+          if (err) { log.info('token server error: ' + err) }
+          if (!payload) { log.info('token server returned empty response') }
+          if (err) { log.info('wreck.read returned an error: ' + err) }
+          log.info('token server response: ' + payload);            
+          log.info('token server response http code: ' + res.statusCode);
+          var pay = JSON.parse(payload);
+          var accessToken = pay['access_token'];
+          log.info('the token is ' + accessToken);
+          // TODO save the token to the user's account in the DB
+          // 2. we have the token. time for to fetch us a lil' profile data.
+          wreck.get(config.get('server.oauth.profileUri'),
+            { headers: {'authorization': 'Bearer ' + accessToken}},
+            function(err, res, payload) {
+              if (err) { log.info('profile server error: ' + err) }
+              if (!payload) { log.info('profile server returned empty response') }
+              // ok so now we've got a profile!
+              log.info('profile server response: ' + payload);
+              // TODO save stuff in the user DB
+              // now let's actually set a proper session cookie, huzzah!
+              var userId = payload.uid;
+              var userEmail = payload.email;
+              request.auth.session.set({fxaId: userId});
+              reply.redirect('/');
+          });
+      });
     }
   }
 }, {
@@ -50,9 +101,10 @@ module.exports = [{
   method: 'GET',
   path: '/dist/{param*}',
   handler: {
+    // TODO use friggin hapi configs before going to production :-P
     directory: {
-      path: config.get('server.staticPath'),
-      listing: config.get('server.staticDirListing')
+      path: path.join(__dirname, '../app'),
+      listing: true
     }
   }
 }];
